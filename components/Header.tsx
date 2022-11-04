81fd7baec5e537fe3d644e3c0b66742a92e014bc
Manash Pratim Pathak <mppoff021@gmail.com>
2022-11-04 10:54:36 +0530

    completed the hero section

--- components/Header.tsx
+++ components/Header.tsx
@@ -8,9 +8,21 @@ export default function Header({}: Props) {
   return (
     <header className="sticky top-0 p-5 flex items-start justify-between max-w-7xl mx-auto z-20 xl:items-center">
         {/* Social-icons */}
-        <motion.div 
+        <motion.div
+        initial={{
+          x  :-500,
+          opacity : 0,
+          scale : 0.5
+        }} 
+        animate = {{
+          x: 0,
+          opacity: 1,
+          scale : 1
+        }}
+        transition={{
+          duration: 1.5
+        }}
         className="flex flex-row items-center"
-        
         >
           <SocialIcon
             url="https://twitter.com/ManashPratimPa5"
@@ -28,7 +40,22 @@ export default function Header({}: Props) {
             bgColor="transparent"
           />
         </motion.div>
-        <div className="flex flex-row items-center text-gray-300 cursor-pointer">
+        <motion.div
+         initial={{
+          x  :500,
+          opacity : 0,
+          scale : 0.5
+        }} 
+        animate = {{
+          x: 0,
+          opacity: 1,
+          scale : 1
+        }}
+        transition={{
+          duration: 1.5
+        }}
+         className="flex flex-row items-center text-gray-300 cursor-pointer"
+         >
             <SocialIcon
             className="cursor-pointer"
             network="email"
@@ -36,7 +63,7 @@ export default function Header({}: Props) {
             bgColor="transparent" 
             />
             <p className="uppercase hidden md:inline-flex text-sm text-gray-400">Get in touch</p>
-        </div>
+        </motion.div>
     </header>
   );
 }
