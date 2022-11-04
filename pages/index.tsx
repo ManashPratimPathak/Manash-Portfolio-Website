0678fb757cdbe2c9695b0a2709687674b7625bf0
Manash Pratim Pathak <mppoff021@gmail.com>
2022-11-04 12:28:58 +0530

    addded circle animation

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,10 @@
 import Head from 'next/head'
 import Header from '../components/Header'
+import Hero from '../components/Hero'
 
 export default function Home() {
   return (
-    <div>
+    <div className='bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0'>
       <Head>
         <title>Manash's Portfolio</title>
       </Head>
@@ -12,6 +13,9 @@ export default function Home() {
        <Header />
 
        {/* hero */}
+       <section id="hero" className='snap-center'>
+        <Hero />
+       </section>
 
        {/* about */}
 
