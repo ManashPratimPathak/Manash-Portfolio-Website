ea64540d3a926aea05623a575344ff7de1587792
Manash Pratim Pathak <mppoff021@gmail.com>
2022-12-20 16:39:09 +0530

    added skills section

---
+++ components/Projects.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+import {motion} from "framer-motion"
+
+type Props = {};
+
+function Projects({}: Props) {
+  const projects = [1, 2, 3, 4, 5];
+  return (
+    <motion.div
+    initial={{opacity: 0}}
+    whileInView={{opacity: 1}}
+    transition={{duration: 1.5}}
+     className="h-screen relative flex overflow-hidden flex-col text-left md:flex-row max-w-full justify-evenly mx-auto items-center z-0">
+      <h3 className="absolute top-24 uppercase tacking-[20px] text-gray-500 text-2xl">
+        Projects
+      </h3>
+
+      <div className="relative w-full flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-20">
+        {projects.map((project, i) => (
+          <div className=" w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-center p-20 md:p-44 h-screen">
+            <motion.img
+            initial={{
+                y:-300,
+                opacity:0
+            }}
+            transition={{duration:1.2}}
+            whileInView={{opacity:1,y:0}}
+            viewport={{once:true}}
+              src="https://cdn.pixabay.com/photo/2016/11/29/12/13/fence-1869401_640.jpg"
+              alt=""
+            ></motion.img>
+            <div className="space-y-10 px=0 md:px-10 max-w-6xl">
+              <h4 className="text-2xl font-semibold text-center">
+                {" "}
+                <span className="underline decoration-[#f7ab0a]/50"> Case study {i + 1} 0f {projects.length}: UPS clone</span>
+              </h4>
+              <p className="text-lg text-center md:text-left">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type</p>
+            </div>
+          </div>
+        ))}
+      </div>
+
+      <div className="w-full absolute top-[30%] bg-[#f7ab0a]/10 left-0 h-[400px] -skew-y-12"></div>
+    </motion.div>
+  );
+}
+
+export default Projects;
+// flex flex-col items-center justify-center w-[300px] h-[300px] bg-[#333333] rounded-xl m-10
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,13 +3,16 @@ import Header from '../components/Header'
 import Hero from '../components/Hero'
 import About from '../components/About'
 import Experience from '../components/Experience'
+import Skills from '../components/Skills'
+import Projects from '../components/Projects'
+import ContactMe from '../components/ContactMe'
 
 export default function Home() {
   return (
     <div className='bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-x-hidden z-0'>
 
       <Head>
-        <title>Manash's Portfolio</title>
+        <title>Manash &apos; s Portfolio</title>
       </Head>
 
        <Header />
@@ -28,10 +31,19 @@ export default function Home() {
        </section>
 
        {/* skills */}
+       <section id="skills" className='snap-start'>
+        <Skills />
+       </section>
 
        {/* projects */}
+       <section id="projects" className='snap-start'>
+        <Projects />
+       </section>
 
        {/* contact me */}
+       <section>
+        <ContactMe />
+       </section>
     </div>
   )
 }
